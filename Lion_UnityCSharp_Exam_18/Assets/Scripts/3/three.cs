35a0da8f2124c7a6c17c5a858efa2c2cf469ee24
afjke36900 <55038688+afjke36900@users.noreply.github.com>
2020-07-21 15:06:36 +0800

    輸出網頁版

---
+++ Lion_UnityCSharp_Exam_18/Assets/Scripts/3/three.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class three : MonoBehaviour
+{
+    public GameObject cube;
+
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < (i+1); j++)
+            {
+                Vector3 pos = new Vector3(i * 1, j * 1, 0);
+                Instantiate(cube, pos, Quaternion.identity);
+            }            
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < (i+1); j++)
+            {
+                Vector3 pos = new Vector3(i * 1, j * -1, 0);
+                Instantiate(cube, pos, Quaternion.identity);
+            }
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < (i + 1); j++)
+            {
+                Vector3 pos2 = new Vector3(i * -1, j * 1, 0);
+                Instantiate(cube, pos2, Quaternion.identity);
+            }
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < (i + 1); j++)
+            {
+                Vector3 pos2 = new Vector3(i * -1, j * -1, 0);
+                Instantiate(cube, pos2, Quaternion.identity);
+            }
+        }
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Lion_UnityCSharp_Exam_18/Assets/Scripts/4/Runner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : MonoBehaviour
+{
+    private Vector3 direction;
+    public float speed = 10f;
+
+    public void Move()
+    {
+
+        //浮點數 前後值 = 輸入類別.取得軸向("垂直") 垂直WS上下
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+
+        if (v == 1) direction = new Vector3(0, 0, 0);
+        if (v == -1) direction = new Vector3(0, 180, 0);
+        if (h == 1) direction = new Vector3(0, 90, 0);
+        if (h == -1) direction = new Vector3(0, 270, 0);
+
+        transform.eulerAngles = direction;
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+}
